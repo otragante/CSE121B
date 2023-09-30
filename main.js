5be04b3f01220e7e1e606afe9e3ae67d0354dbba
otragante <95303001+otragante@users.noreply.github.com>
2023-09-30 18:44:25 +0100

    Fix main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+// copyright year
+document.querySelector('#year').textContent = new Date().getFullYear();
+
+// toggle menu in small view
+const toggleMenu = () => {
+    document.querySelector('#menu').classList.toggle('open');
+}
+
+document.querySelector('#toggleMenu').addEventListener('click', toggleMenu);
